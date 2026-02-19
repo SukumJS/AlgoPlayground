4123bc9e5601e6bfc0b383f0d750a4ae1872cd17
watcharin28 <phon567gjo@gmail.com>
2026-02-19 14:23:21 +0700

    add status color

--- src/components/shared/customNode.tsx
+++ src/components/shared/customNode.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import type { Node, NodeProps } from '@xyflow/react';
 
-export default function CustomNode({ data }: NodeProps<Node<{ label: string }>>) {
-    return (
-        <div className="shrink-0 flex text-[#222121] text-2xl font-semibold justify-center items-center border-2 border-[#5D5D5D] bg-[#D9E363] w-14 h-14 rounded-lg cursor-grab">
-            {data.label}
-        </div>
-    );
+import type { NodeProps } from "@xyflow/react";
+import type { SortNodeData } from "@/src/components/shared/sortNode";
+const STATUS_BG: Record<SortNodeData["status"], string> = {
+  idle: "#D9E363",
+  compare: "#62A2F7",
+  swap: "#F19F72",
+  sorted: "#D9E363",
+  merge: "#B784F7",
+};
+
+export default function CustomNode({ data }: NodeProps) {
+  const nodeData = data as SortNodeData;
+
+  return (
+    <div
+      className="
+        flex text-[#222121] text-2xl font-semibold
+        justify-center items-center border-2 border-[#5D5D5D]
+        w-14 h-14 rounded-lg cursor-grab
+        transition-transform duration-300 ease-in-out
+      "
+      style={{
+        backgroundColor: STATUS_BG[nodeData.status],
+      }}
+    >
+      {nodeData.value}
+    </div>
+  );
 }
+
+
