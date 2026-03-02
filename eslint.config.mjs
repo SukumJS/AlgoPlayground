aefa6c22d7245fc04c616bb7629dfeaa543e60ce
Sukum <sukumpat2547@gmail.com>
2026-03-02 15:28:42 +0700

    Config Eslint Prettier and validator

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,12 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import prettierConfig from 'eslint-config-prettier';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-});
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = [
-    ...compat.extends('next/core-web-vitals', 'next/typescript'),
-    prettierConfig,
+  { ignores: ["types/**"] },
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  prettierConfig,
 ];
 
 export default eslintConfig;
